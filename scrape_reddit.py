c8d086cad55e7b5c09a02121db8d80a0a916498f
Brian McKay <brian.mckay@doestech.com>
2023-04-02 19:50:08 -0700

    Updated redgifs for better capture

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -33,8 +33,7 @@ baseHeaders['User-Agent']='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) Apple
 
 gifheaders = {}
 gifheaders['User-Agent']='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-gifheaders['authorization']='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy5yZWRnaWZzLmNvbS8iLCJpYXQiOjE2ODAzNzM5NDYsImV4cCI6MTY4MDQ2MDM0Niwic3ViIjoiY2xpZW50LzE4MjNjMzFmN2QzLTc0NWEtNjU4OS0wMDA1LWQ4ZThmZTBhNDRjMiIsInNjb3BlcyI6InJlYWQiLCJ2YWxpZF9hZGRyIjoiOTguMTY1LjMyLjIzMyIsInZhbGlkX2FnZW50IjoiTW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTBfMTVfNykgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMS4wLjAuMCBTYWZhcmkvNTM3LjM2IiwicmF0ZSI6LTF9.GvjQr2Tqd97uT3R267X-krmvm-DYWaJMlmQ3iVV3UAzvN6-u8WaKvj76ua-ECEtXxKWNKTiNupW5_mtKsprKKjl_6vjlpf7-uBramN6c3qd286AdW9pnkA1YL9_CoBvYgNnsb5H4x_HoVoCjC_iUw_YKn1sEH65Z_64a_zrtkmT0mSzCEaSWgiIguadjfwLmOJ6lLD7MCCqrmg2aBzyNjBKsQij5f2aEE_XF3feZFs8kADzx_URbAGXhsNZMbQFNh9EgrfO1UnHsONGcowABXgusplay8-2iZHoB-WlfkuyzVmtOL5mMadXuj4RdCI2Qe63B3yEbHIJbJRKTTnf5oA'
-
+gifheaders['authorization']='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJodHRwczovL3d3dy5yZWRnaWZzLmNvbS8iLCJpYXQiOjE2ODA0NjA4NTMsImV4cCI6MTY4MDU0NzI1Mywic3ViIjoiY2xpZW50LzE4MjNjMzFmN2QzLTc0NWEtNjU4OS0wMDA1LWQ4ZThmZTBhNDRjMiIsInNjb3BlcyI6InJlYWQiLCJ2YWxpZF9hZGRyIjoiOTguMTY1LjMyLjIzMyIsInZhbGlkX2FnZW50IjoiTW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTBfMTVfNykgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMS4wLjAuMCBTYWZhcmkvNTM3LjM2IiwicmF0ZSI6LTF9.kAmzH47Lor2MIZvtmZ07WxLgjVWbu45nxkJJ5zAy0d3KdEjtXIoNVV_OvWNSNqlFGG-dInsN1e2i84z2EH-kFj1ZBSyv7pC9UO9wjAl1b_1FYPwP7PCXu9hK44Fzmg7W_ZcUrUCF_5ngi1wOdfsmyaD04Qjt5LinqKje4eVODT0Jzm2By_ckHVXxEFt7oQ_VRlSWr2YsVVObOu6FLgfI3ft-4lXgtqQCpm4Knpd3MNSCslXfcGFrLTJ7mc6xgyJo4dAszuDYvwWR9MkzIIhlAe5l-of7S2LzBtu8Z0rMB7UWZge985NRSuKG7Tj_V6PavRpBPjtqiyAJURU6nVFRvg'
 
 def writeImageOut(filename, url, head=baseHeaders):
 	global sleepy, successCount
@@ -99,7 +98,7 @@ for x in links:
 				if ('error' in initialRequest.text):
 					raise Exception(initialRequest.text)
 				finalRequest = re.findall('(?i)"hd":"(.*?' + gfycatRE[0][1] + '\.(.*?)\?.*?)"', initialRequest.text)[0]
-				writeImageOut(x[0] + ' - ' + x[1] + ' - ' + redgifsRE[0][1] + '.' + finalRequest[1], finalRequest[0], gifheaders)
+				writeImageOut(x[0] + ' - ' + x[1] + ' - ' + gfycatRE[0][1] + '.' + finalRequest[1], finalRequest[0], gifheaders)
 		elif ('redgifs' in x[2]):
 			print('redgif processing: ' + str(x))
 			redgifsRE = re.findall('.*\/(.*)',x[2])
@@ -118,8 +117,7 @@ for x in links:
 				if (imgurRE):
 					writeImageOut(x[0] + ' - ' + x[1] + ' - ' + imgurRE[0][1], imgurRE[0][0] + imgurRE[0][1])
 				else:
-					print(requests.get(x[2]).text)
-					raise Exception("Unable to find imgur image.")
+					raise Exception("Unable to find imgur image. Likely gone forever. Returned " + requests.get(x[2]).text[0:5])
 		elif ('i.redd.it' in x[2]):
 			print('redd.it processing: ' + str(x))
 			writeImageOut(x[0] + ' - ' + x[1] + ' - ' + x[3] + x[5] + '.' + x[4] + x[6], x[2])
